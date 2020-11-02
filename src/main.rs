948c8e104e9df22c54a4af1c18d3805d6616f8bf
Aaron Korte <github@aaronkorte.xyz>
2020-11-02 23:32:53 +0100

    More trial and error
    Slightly change scatter render.
    Move sampling to function.
    Include FFT lib for possible future use?

--- src/main.rs
+++ src/main.rs
@@ -5,43 +5,86 @@ use plotlib::repr::Plot;
 use plotlib::style::{PointMarker, PointStyle};
 use plotlib::view::ContinuousView;
 
+use rustfft::num_complex::Complex;
+use rustfft::num_traits::Zero;
+use rustfft::FFTplanner;
+
 pub mod parser;
 
 fn main() {
     println!("Moin!");
 
-    //render_scatter();
-
-    morse_to_ascii();
+    try_find_beeps();
 }
 
-fn morse_to_ascii() {
-    println!("A: {}", parser::to_ascii(vec![parser::MorseToken::Short, parser::MorseToken::Long]).unwrap());
+fn try_find_beeps() {
+    let samples = get_indexed_samples("samples/marc01.wav", 1);
+    println!("Min: {}", samples.iter().map(|(_, s)| s).min().unwrap());
+    println!("Max: {}", samples.iter().map(|(_, s)| s).max().unwrap());
+}
+
+fn run_fft() {
+    let mut reader = hound::WavReader::open("samples/marc01.wav").unwrap();
+    let all_samples = reader.samples::<i16>().map(|s| s.unwrap() as f32);
+    println!("Number of samples in file: {}", all_samples.len());
+
+    let mut input: Vec<Complex<f32>> = all_samples.map(|f| Complex::new(f, 0.)).collect();
+    let mut output: Vec<Complex<f32>> = vec![Complex::zero(); 249824];
+
+    let mut planner = FFTplanner::new(false);
+    let fft = planner.plan_fft(249824);
+    fft.process(&mut input, &mut output);
+
+    println!("Foo!");
 }
 
 fn render_scatter() {
-    let mut reader = hound::WavReader::open("samples/marc01.wav").unwrap();
-    let all_samples = reader.samples::<i16>();
-    println!("Number of samples in file: {}", all_samples.len());
+    let all_samples = get_indexed_samples("samples/marc01.wav", 10);
+
+    let num_of_samples = all_samples.len();
 
     let indexed_samples = all_samples
-        .enumerate()
-        .map(|(i, s)| (i as f64, s.unwrap() as f64))
+        .iter()
+        .map(|(i, s)| (*i as f64, *s as f64))
         .collect();
-    
+
     let s1: Plot = Plot::new(indexed_samples).point_style(
         PointStyle::new()
             .marker(PointMarker::Circle)
-            .size(0.1)
+            .size(0.3)
             .colour("#DD3355"),
     );
 
     let v = ContinuousView::new()
         .add(s1)
-        .x_range(0., 250000.)
-        .y_range(-130., 130.)
-        .x_label("index")
+        .x_range(0., num_of_samples as f64)
+        //.y_range(-262144., 262144.)
+        .y_range(-128., 128.)
+        .x_label("sample")
         .y_label("value");
 
     Page::single(&v).save("scatter.svg").unwrap();
+}
+
+type Sample = (u32, i32);
+fn get_indexed_samples<'a>(filename: &str, resolution: u32) -> Vec<Sample> {
+    let mut reader = hound::WavReader::open(filename).unwrap();
+    let all_samples = reader.samples::<i32>();
+    let number = all_samples.len() as u32;
+
+    println!("Actual number of samples in file: {}", number);
+    println!(
+        "Filtered number of samples in file: {}",
+        number / resolution
+    );
+
+    let temp = all_samples
+        .enumerate()
+        .map(|(i, s)| (i as u32, s.unwrap() as i32));
+
+    if resolution > 1 {
+        temp.filter(|(i, _)| i % resolution == 0).collect()
+    } else {
+        temp.collect()
+    }
 }
