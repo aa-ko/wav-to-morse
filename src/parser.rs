1254493fdc94a78eedefe18632883c226152ae34
Aaron Korte <github@aaronkorte.xyz>
2020-11-03 16:49:28 +0100

    Remove Nop
    Nop is no longer a valid instruction.
    Replaced all Nops with short or long pauses, seperating chars and words respectively.

--- src/parser.rs
+++ src/parser.rs
@@ -1,26 +1,36 @@
 // TODO: Actually use iterators instead of calling collect all the time.
 
+#[derive(PartialEq)]
 pub enum MorseToken {
     Short,
     Long,
-    Nop,
+    ShortPause,
+    LongPause,
+    Error,
 }
 
 use std::fmt;
 impl fmt::Display for MorseToken {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", match self {
-            MorseToken::Short => "Short",
-            MorseToken::Long => "Long",
-            MorseToken::Nop => "Nop"
-        })
+        write!(
+            f,
+            "{}",
+            match self {
+                MorseToken::Short => "Short",
+                MorseToken::Long => "Long",
+                MorseToken::ShortPause => "ShortPause",
+                MorseToken::LongPause => "LongPause",
+                MorseToken::Error => "Error",
+            }
+        )
     }
 }
 
 enum ParserState {
     Short,
     Long,
-    Pause,
+    ShortPause,
+    LongPause,
 }
 
 struct ParserStateCounter {
@@ -36,55 +46,70 @@ impl ParserStateCounter {
     }
 }
 
+// TODO: Implement single unit length const.
 pub fn translate(quantized_frames: Vec<bool>) -> Option<String> {
+    const half_unit_size: usize = 2;
+
     let mut tokens: Vec<MorseToken> = vec![];
 
     let mut c = ParserStateCounter {
-        state: ParserState::Pause,
+        state: ParserState::ShortPause,
         c_true: 0,
         c_false: 0,
     };
     for f in quantized_frames {
         println!("{}", f);
-        match c.state {
-            ParserState::Short => {
-                if c.c_true >= 4 {
-                    c.state = ParserState::Long;
-                    c.c_false = 0;
-                } else if c.c_false >= 2 {
-                    c.state = ParserState::Pause;
-                    c.reset_c();
-                    tokens.push(MorseToken::Short);
-                }
-            }
-            ParserState::Long => {
-                if c.c_false >= 2 {
-                    c.state = ParserState::Pause;
-                    c.reset_c();
-                    tokens.push(MorseToken::Long);
-                }
-            }
-            ParserState::Pause => {
-                if c.c_true >= 2 {
-                    c.state = ParserState::Short;
-                    tokens.push(MorseToken::Nop);
-                }
-            }
-        }
 
         if f {
             c.c_true += 1;
         } else {
             c.c_false += 1;
         }
+
+        match c.state {
+            ParserState::Short => {
+                if c.c_true >= 3 * half_unit_size {
+                    c.state = ParserState::Long;
+                    c.c_false = 0;
+                } else if c.c_false >= half_unit_size {
+                    c.state = ParserState::ShortPause;
+                    c.reset_c();
+                    tokens.push(MorseToken::Short);
+                }
+            }
+            ParserState::Long => {
+                if c.c_false >= half_unit_size {
+                    c.state = ParserState::ShortPause;
+                    c.reset_c();
+                    tokens.push(MorseToken::Long);
+                }
+            }
+            ParserState::ShortPause => {
+                if c.c_true >= half_unit_size {
+                    c.state = ParserState::Short;
+                    c.c_false = 0;
+                    tokens.push(MorseToken::ShortPause);
+                } else if c.c_false >= 8 * half_unit_size {
+                    c.state = ParserState::LongPause;
+                    c.c_true = 0;
+                }
+            }
+            ParserState::LongPause => {
+                if c.c_true >= half_unit_size {
+                    c.state = ParserState::Short;
+                    tokens.push(MorseToken::LongPause);
+                    c.c_false = 0;
+                }
+            }
+        }
     }
 
-    for d in tokens {
-        println!("{}", d);
-    }
+    // for d in tokens {
+    //     println!("{}", d);
+    // }
 
-    Some("_".to_owned())
-    //to_ascii(tokens)
+    //Some("_".to_owned())
+    to_ascii(tokens)
 }
 
 fn to_ascii(input: Vec<MorseToken>) -> Option<String> {
@@ -102,12 +127,14 @@ fn split_tokens(input: Vec<MorseToken>) -> Vec<Vec<MorseToken>> {
 
     for t in input {
         match t {
-            MorseToken::Nop => {
+            MorseToken::LongPause => {
                 if temp.len() != 0 {
                     result.push(temp);
                     temp = vec![];
+                    result.push(vec![MorseToken::LongPause]);
                 }
             }
+            MorseToken::ShortPause => {}
             _ => temp.push(t),
         }
     }
@@ -121,13 +148,14 @@ fn char_to_token(s: char) -> MorseToken {
     match s {
         '.' => MorseToken::Short,
         '-' => MorseToken::Long,
-        _ => MorseToken::Nop,
+        _ => MorseToken::Error,
     }
 }
 fn token_to_char(t: &MorseToken) -> char {
     match t {
         MorseToken::Short => '.',
         MorseToken::Long => '-',
+        MorseToken::LongPause => ' ',
         _ => '?',
     }
 }
@@ -135,6 +163,10 @@ fn token_to_char(t: &MorseToken) -> char {
 fn decode_tokenstream(tokens: &Vec<MorseToken>) -> char {
     if tokens.len() <= 0 {
         return '_';
+    }
+
+    if tokens.len() == 1 && tokens[0] == MorseToken::LongPause {
+        return ' ';
     }
 
     let key = tokens
