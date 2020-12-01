8a456b191436dc65662c9ea937a4595e17e0c28d
Aaron Korte <github@aaronkorte.xyz>
2020-12-01 21:30:35 +0100

    Try to use traits

--- src/preprocess.rs
+++ src/preprocess.rs
@@ -4,9 +4,50 @@ pub type Sample = (usize, i32);
 pub type Frame = (usize, u32);
 pub type NormalizedFrame = (usize, f64);
 
-pub fn normalize_frames(frames: Vec<Frame>) -> Vec<NormalizedFrame> {
-    let min = frames.iter().map(|(_, s)| s).min().unwrap();
-    let max = frames.iter().map(|(_, s)| s).max().unwrap();
+pub type SampleVec = Vec<Sample>;
+pub type FrameVec = Vec<Frame>;
+pub type NormalizedFrameVec = Vec<NormalizedFrame>;
+
+trait MinMax<T> {
+    fn vecmin(&self) -> T;
+    fn vecmax(&self) -> T;
+}
+
+impl MinMax<i32> for SampleVec {
+    fn vecmin(&self) -> i32 {
+        self.iter().map(|(_, s)| s).min().unwrap().to_owned()
+    }
+
+    fn vecmax(&self) -> i32 {
+        self.iter().map(|(_, s)| s).max().unwrap().to_owned()
+    }
+}
+
+impl MinMax<u32> for FrameVec {
+    fn vecmin(&self) -> u32 {
+        self.iter().map(|(_, s)| s).min().unwrap().to_owned()
+    }
+
+    fn vecmax(&self) -> u32 {
+        self.iter().map(|(_, s)| s).max().unwrap().to_owned()
+    }
+}
+
+// impl MinMax<f64> for NormalizedFrameVec {
+//     fn vecmin(&self) -> f64 {
+//         self.iter().map(|(_, s)| s).min().unwrap().to_owned()
+//     }
+
+//     fn vecmax(&self) -> f64 {
+//         self.iter().map(|(_, s)| s).max().unwrap().to_owned()
+//     }
+// }
+
+pub fn normalize_frames(frames: FrameVec) -> NormalizedFrameVec {
+    //let min = frames.iter().map(|(_, s)| s).min().unwrap();
+    //let max = frames.iter().map(|(_, s)| s).max().unwrap();
+    let min = frames.vecmin();
+    let max = frames.vecmax();
     let div = (max - min) as f64;
     frames
         .iter()
@@ -14,7 +55,7 @@ pub fn normalize_frames(frames: Vec<Frame>) -> Vec<NormalizedFrame> {
         .collect()
 }
 
-pub fn get_indexed_samples(filename: &str, resolution: usize) -> Vec<Sample> {
+pub fn get_indexed_samples(filename: &str, resolution: usize) -> SampleVec {
     let mut reader = hound::WavReader::open(filename).unwrap();
     let wav_samples = reader.samples::<i32>();
     let samples_len = wav_samples.len();
@@ -25,7 +66,7 @@ pub fn get_indexed_samples(filename: &str, resolution: usize) -> Vec<Sample> {
         samples_len / resolution
     );
     
-    let samples: Vec<Sample> = wav_samples.enumerate().map(|(i, s)| (i, s.unwrap())).collect();
+    let samples: SampleVec = wav_samples.enumerate().map(|(i, s)| (i, s.unwrap())).collect();
     
     println!("Min: {}", samples.iter().map(|(_, s)| s).min().unwrap());
     println!("Max: {}", samples.iter().map(|(_, s)| s).max().unwrap());
